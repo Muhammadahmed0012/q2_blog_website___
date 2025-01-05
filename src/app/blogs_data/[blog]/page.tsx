7ba31d548958d98e9cf0b706bc4bc02046a94eb8
Muhammadahmed0012 <159796230+Muhammadahmed0012@users.noreply.github.com>
2025-01-06 02:24:57 +0500

    commit

--- src/app/blogs_data/[blog]/page.tsx
+++ src/app/blogs_data/[blog]/page.tsx
@@ -2,6 +2,7 @@ import { client } from "@/sanity/lib/client";
 import React from "react";
 import Image from "next/image";
 import { urlFor } from "@/sanity/lib/image";
+import CommentSection from "@/components/coments";
 // interface Iblog {
 //   blog: string;
 // }
@@ -24,18 +25,21 @@ export default async function Blog({params}: {params: Promise<{blog : string }>}
   return (
     <div className="">
      <div className="flex flex-col items-center space-y-5  ">
-      <Image
-        src={urlFor(query.blog_image).url()}
-        alt="img"
-        height={100}
-        width={100}
-       className="w-[60rem] h-[500px]"
-      />
+     <Image
+  src={urlFor(query.blog_image).url()}
+  alt="Blog Image"
+  height={400}
+  width={900}
+  className="w-full max-w-[900px] h-[300px] rounded-lg shadow-md"
+/>
+
+      
       
 
       <span>{query.author_name}</span> 
        <h2>{query.heading}</h2>
        <p>{query.description2}</p>
+       <CommentSection/>
        </div>
     </div>
   );
